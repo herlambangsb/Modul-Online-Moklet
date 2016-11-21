9e38801f51a8bbda2764c0f1297b61d3ad50545b
Coldbrain <kamandaka25@gmail.com>
2016-11-21 19:26:39 +0700

    - about us selesai

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl209182736/modulonlinemoklet/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl209182736/modulonlinemoklet/MainActivity.java
@@ -4,11 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageButton imgmodul, imgabout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
