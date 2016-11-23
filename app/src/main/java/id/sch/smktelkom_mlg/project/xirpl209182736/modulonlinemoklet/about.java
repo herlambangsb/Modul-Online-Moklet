8459c38797a61248936cff5c1c7cf46d7801738f
Herlambangsb <herlambangsb>
2016-11-23 10:49:59 +0700

    Layout Akhir 2

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl209182736/modulonlinemoklet/about.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl209182736/modulonlinemoklet/about.java
@@ -1,9 +1,7 @@
 package id.sch.smktelkom_mlg.project.xirpl209182736.modulonlinemoklet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class about extends AppCompatActivity {
 
@@ -12,30 +10,6 @@ public class about extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        findViewById(R.id.aboutasha).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(about.this, asha.class));
-            }
-        });
 
-        findViewById(R.id.aboutaji).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(about.this, aji.class));
-            }
-        });
-        findViewById(R.id.aboutdesta).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(about.this, desta.class));
-            }
-        });
-        findViewById(R.id.aboutsadil).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(about.this, sadil.class));
-            }
-        });
     }
 }
